4778a51ffa3dd70253e85aa8fdda88608b830372
longguru <lovanlong@amycos.vn>
2024-07-22 21:32:37 +0700

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/tailwindcss'],
+  modules: [ '@nuxtjs/seo', '@nuxtjs/tailwindcss', '@nuxtjs/tailwindcss'],
   googleFonts: {
     families: {
       Lato: [100, 300, 400, 700, 900],
