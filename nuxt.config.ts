d929bae1296670c3a427a25044c741f3bd47279d
longguru <lovanlong@amycos.vn>
2024-07-22 21:51:56 +0700

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,13 @@ export default defineNuxtConfig({
       Lato: [100, 300, 400, 700, 900],
     },
   },
+  site: {
+    url: 'https://www.longwebstudio.net/',
+    name: `Lỗ Văn Long - Nhà Thiết Kế Website Hiện Đại`,
+    description: `Tôi là nhà thiết kế website hiện đại và chuyên nghiệp, cung cấp các giải pháp thiết kế như website cửa hàng, thiết kế website blog, và ứng dụng.`,
+    defaultLocale: 'vi'
+  },
+
   gtag: {
     id: 'G-7NHZPV6N6F'
   }
